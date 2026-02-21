e5e4c0e43e9f7e50d67c9130816502843d33a9ed
Pedro <jpassesoriaprevidenciaria@gmail.com>
2026-02-20 22:29:18 -0300

    Fix build error dashboard

--- core/views.py
+++ core/views.py
@@ -3,8 +3,9 @@ from .models import Cliente
 from .forms import ClienteForm
 
 def home(request):
-    total_clientes = Cliente.objects.count()
-    ultimos_clientes = Cliente.objects.all().order_by('-id')[:5]
+    clientes = Cliente.objects.all().order_by('-id')
+    total_clientes = clientes.count()
+    ultimos_clientes = clientes[:5]
 
     context = {
         'total_clientes': total_clientes,
@@ -12,3 +13,16 @@ def home(request):
     }
 
     return render(request, 'home.html', context)
+
+
+def lista_clientes(request):
+    clientes = Cliente.objects.all().order_by('-id')
+    return render(request, 'clientes/lista_cliente.html', {'clientes': clientes})
+
+
+def cadastrar_cliente(request):
+    form = ClienteForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('lista_clientes')
+    return render(request, 'clientes/cadastra_cliente.html', {'form': form})
